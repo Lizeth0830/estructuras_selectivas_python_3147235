98c27c618c9717206c3a568eb1d54ce19441ac42
tomasgarciaguevara <tomasgarcia1826@gmail>
2025-03-19 10:25:51 -0500

    Subidas Frutas

---
+++ if_cascada.py
@@ -0,0 +1,41 @@
+'''
+If en cascada:
+Es una estructura selectiva
+compuesta por multiples
+condicionales, seguidos 
+unos de otros
+Sintaxis:
+if condicional1:
+    instruccion1
+    instruccion2
+elif condicional2:
+    instruccion3
+    instruccion4
+elif condicional3:
+    instruccion5
+    instruccion6
+NOTA: Cada condicional
+es MUTUAMENTE EXCLUYENTE
+'''
+#Ejemplo:
+#Vamos a hacer un pequeño 
+#traductor.
+#el programa debe permitir
+#leer una fruta en español
+#y debe mostrar esa fruta 
+#en ingles
+Fruta_es = input("Ingrese Una Fruta: ")
+if Fruta_es == "manzana" or Fruta_es == "Manzana":
+    print("Apple")
+elif Fruta_es == "naranja" or Fruta_es == "Naranja":
+    print("Orange")
+elif Fruta_es == "uva" or Fruta_es == "Uva":
+    print("Grape")
+elif Fruta_es == "mandarina" or Fruta_es == "Mandarina":
+    print("Tangerine")
+elif Fruta_es == "banano" or Fruta_es == "Banano":
+    print("Banana")
+#Caso por defecto
+#default
+else: 
+    print("No Se Encontró Traducción")
